38936929a73a33d2dc7f5cafbc8f0f82de4e7a23
dinesh <dgoud99120@gmail.com>
2023-08-15 14:57:04 +0530

    a

---
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -0,0 +1,143 @@
+import { getClient } from "@/apollo-client";
+
+import CalloutCard from "@/components/CalloutCard";
+import HumidityChart from "@/components/HumidityChart";
+import InformationPanel from "@/components/InformationPanel";
+import RainChart from "@/components/RainChart";
+import StatCard from "@/components/StatCard";
+import TempChart from "@/components/TempChart";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+import cleanData from "@/lib/cleanData";
+import getBasePath from "@/lib/getBasePath";
+
+export const revalidate = 60;
+
+type Props = {
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
+};
+
+async function Weatherpage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      latitude: lat,
+      longitude: long,
+      // relative to me
+      timezone: "PST",
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  const dataToSend = cleanData(results, city);
+
+  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      weatherData: dataToSend
+    })
+  })
+
+  const GPTdata = await res.json();
+  const { content } = GPTdata;
+
+  return (
+    <div className="flex flex-col min-h-screen md:flex-row">
+      <InformationPanel
+        city={city}
+        lat={Number(lat).toFixed(2)}
+        long={Number(long).toFixed(2)}
+        results={results}
+      />
+
+      <div className="flex-1 p-5 lg:p-10">
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <p className="text-sm text-gray-400">Last Updated at: {" "}
+              {new Date(results.current_weather.time).toLocaleString()}({results.timezone})
+            </p>
+          </div>
+
+          <div className="m-2 mb-10">
+            <CalloutCard
+              message={content}
+            />
+          </div>
+
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+            <StatCard
+              title="Maximum Temperature"
+              metric={`${(results.daily.temperature_2m_max[0] * (9 / 5) + 32).toFixed(1)}°F`}
+              color="yellow"
+            />
+
+            <StatCard
+              title="Minimum Temperature"
+              metric={`${(results.daily.temperature_2m_min[0] * (9 / 5) + 32).toFixed(1)}°F`}
+              color="green"
+            />
+
+            <div>
+              <StatCard
+                title="UV Index Clear Sky"
+                metric={results.daily.uv_index_clear_sky_max[0].toFixed(1)}
+                color="rose"
+              />
+              {Number(results.daily.uv_index_clear_sky_max[0].toFixed(1)) > 5 && (
+                <CalloutCard
+                  message={"The UV is high today, be sure to wear SPF!"}
+                  warning
+                />
+              )}
+            </div>
+
+            <div className="flex space-x-3">
+              <StatCard
+                title="Wind Speed"
+                metric={`${(results.current_weather.windspeed).toFixed(1)} mph`}
+                color="cyan"
+              />
+
+              <StatCard
+                title="Wind Direction"
+                subtitle="N-0°, E-90°, S-180°, & W-270°"
+                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                color="violet"
+              />
+            </div>
+
+          </div>
+        </div>
+
+        <hr className="mb-5" />
+
+        <div className="space-y-3">
+          <TempChart results={results} />
+          <RainChart results={results} />
+          <HumidityChart results={results} />
+        </div>
+
+        {/* <p>Welcome to the weather page {city} {lat} {long}</p> */}
+        {/* <br /> */}
+        {/* Verifies that the endpt query functions as expected */}
+        {/* <p>
+          {JSON.stringify(results)}
+        </p> */}
+
+      </div>
+    </div>
+  )
+}
+
+export default Weatherpage
---
+++ components/CityPicker.tsx
@@ -0,0 +1,180 @@
+"use client"
+
+import { Country, City, State } from "country-state-city"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import Select from "react-select"
+import { GlobeIcon } from "@heroicons/react/solid"
+
+type countryOption = {
+  value: {
+    latitide: string,
+    longitude: string,
+    isoCode: string,
+    name: string,
+  };
+  label: string;
+} | null;
+
+type stateOption = {
+  value: {
+    latitide: string | null | undefined,
+    longitude: string | null | undefined,
+    isoCode: string,
+    countryCode: string,
+    name: string,
+  };
+  label: string;
+} | undefined | null;
+
+type cityOption = {
+  value: {
+    latitide: string | null | undefined,
+    longitude: string | null | undefined,
+    countryCode: string,
+    name: string,
+    stateCode: string,
+  };
+  label: string;
+} | undefined | null;
+
+const countryOptions = Country.getAllCountries().map(country => ({
+  value: {
+    latitide: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+    name: country.name,
+  },
+  label: country.name,
+}))
+
+const CountriesWithStates = new Set<String>([
+  "AU",
+  "AT",
+  "BR",
+  "DE",
+  "FM",
+  "IN",
+  "MM",
+  "MX",
+  "MY",
+  "NZ",
+  "NG",
+  "PW",
+  "SS",
+  "US",
+]);
+
+const CityPicker = () => {
+  const [selectedCountry, setSelectedCountry] = useState<countryOption>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const [selectedState, setSelectedState] = useState<stateOption>(null);
+  const isCountryWithState = CountriesWithStates.has(selectedCountry?.value?.isoCode || '');
+
+  const router = useRouter();
+
+  const handleSelectedCountry = (option: countryOption) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+    setSelectedState(null);
+  }
+
+  const handleSelectedState = (option: stateOption) => {
+    setSelectedState(option);
+    setSelectedCity(null);
+  }
+
+  const handleSelectedCity = (option: cityOption) => {
+    setSelectedCity(option);
+    router.push(`/location/${option?.value.name}/${option?.value.latitide}/${option?.value.longitude}`)
+  }
+
+  const getStateOptions = (): any[] => {
+    let options = State.getStatesOfCountry(selectedCountry?.value?.isoCode || '') || [];
+    return options.map(state => ({
+      value: {
+        name: state.name,
+        isoCode: state.isoCode,
+        countryCode: state.countryCode,
+        latitude: state.latitude,
+        longitude: state.longitude,
+      },
+      label: state.name,
+    }))
+  }
+
+  const getCityOptions = (): any[] => {
+    let options = [];
+
+    if (isCountryWithState) {
+      options = City.getCitiesOfState(selectedState?.value?.countryCode || '', selectedState?.value.isoCode || '') || [];
+    } else {
+      options = City.getCitiesOfCountry(selectedCountry?.value?.isoCode || '') || [];
+    }
+    return options.map(city => ({
+      value: {
+        latitide: city.latitude,
+        longitude: city.longitude,
+        countryCode: city.countryCode,
+        name: city.name,
+        stateCode: city.stateCode,
+      },
+      label: city.name,
+    }));
+  }
+
+  return (
+    <div className="space-y-4">
+      <div className="space-y-2">
+        <div className="flex items-center space-x-2 text-white/80">
+          <GlobeIcon className="h-5 w-5 text-white" />
+          <label htmlFor="country">Country</label>
+        </div>
+        <Select
+          className="text-black"
+          value={selectedCountry}
+          onChange={handleSelectedCountry}
+          options={countryOptions}
+          id="countryName"
+        />
+      </div>
+
+      {selectedCountry && isCountryWithState && (
+        <div className="space-y-2">
+          <div className="flex items-center space-x-2 text-white/80">
+            <GlobeIcon className="h-5 w-5 text-white" />
+            <label htmlFor="state">State</label>
+          </div>
+          <Select
+            className="text-black"
+            value={selectedState}
+            onChange={handleSelectedState}
+            options={getStateOptions()}
+            id="stateName"
+          />
+        </div>
+      )}
+
+      {((selectedCountry && isCountryWithState && selectedState) ||
+        (selectedCountry && !isCountryWithState)) && (
+          <div className="space-y-2">
+            <div className="flex items-center space-x-2 text-white/80">
+              <GlobeIcon className="h-5 w-5 text-white" />
+              <label htmlFor="city">City</label>
+            </div>
+            <Select
+              className="text-black"
+              value={selectedCity}
+              onChange={handleSelectedCity}
+              options={getCityOptions()}
+              id="cityName"
+            />
+          </div>
+        )
+      }
+
+    </div>
+  )
+}
+
+export default CityPicker
